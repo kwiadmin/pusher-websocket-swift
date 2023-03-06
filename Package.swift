c8af0a628bbb31ff79be3f27482ce0ec48f44544
Omran Khoja <33832722+OmranK@users.noreply.github.com>
2023-03-06 16:06:30 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
         .library(name: "PusherSwift", targets: ["PusherSwift"])
     ],
     dependencies: [
-        .package(url: "https://github.com/pusher/NWWebSocket.git", .upToNextMajor(from: "0.5.2")),
-        .package(url: "https://github.com/bitmark-inc/tweetnacl-swiftwrap", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/kwiadmin/NWWebSocket", .upToNextMajor(from: "0.5.2")),
+        .package(url: "https://github.com/kwiadmin/tweetnacl-swiftwrap", .upToNextMajor(from: "1.0.0")),
     ],
     targets: [
         .target(
